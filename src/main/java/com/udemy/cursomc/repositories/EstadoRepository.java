8d8ff1bb41e01308436e461371eb548ecf3dbe94
Bruno Zani <ubzani@gmail.com>
2018-04-23 22:03:20 -0300

    Estado e Cidade

---
+++ src/main/java/com/udemy/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.udemy.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.udemy.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+	// Categoria -> objeto a trabalhar repository
+	// Integer -> tipo do atributo identificador, no caso "id"
+}
