543834f287d2496adf20d3e13fda44b200140a11
Bruno Zani <ubzani@gmail.com>
2018-05-01 15:09:11 -0300

    Validacao customizada: email nao repetido na insercao de cliente

--- src/main/java/com/udemy/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/udemy/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.udemy.cursomc.domain.Cliente;
 import com.udemy.cursomc.domain.enums.TipoCliente;
 import com.udemy.cursomc.dto.ClienteNewDTO;
+import com.udemy.cursomc.repositories.ClienteRepository;
 import com.udemy.cursomc.resources.exception.FieldMessage;
 import com.udemy.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -30,6 +37,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		// varre erros
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
