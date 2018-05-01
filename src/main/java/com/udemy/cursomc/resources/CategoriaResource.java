c0b0d03428d8f2067bc52c1a36a4aca3145907f3
Bruno Zani <ubzani@gmail.com>
2018-05-01 01:24:51 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,6 +28,9 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+			// PathVariable -> linka id do metodo (parametro) com id do endpoint (value)
+			// ResponseEntity<?> -> encapsula uma resposta http para o tipo rest
+			//		    torna generico, ao inves de retornar lista de categorias 
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
@@ -61,8 +66,16 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> find(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+				// formato: /categorias/page?page=0&linesPerPage=20&...
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj)); 
+		return ResponseEntity.ok().body(listDto);
+	}
+	
 }
-
-// PathVariable -> linka id do metodo (parametro) com id do endpoint (value)
-// ResponseEntity<?> -> encapsula uma resposta http para o tipo rest
-//    torna generico, ao inves de retornar lista de categorias 
