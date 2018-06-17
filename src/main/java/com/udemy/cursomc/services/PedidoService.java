0d15406788aff835e18fe10a897aaebe0d2c5b65
Bruno Zani <ubzani@gmail.com>
2018-06-17 12:26:36 -0300

    Implementando toString de Pedido

--- src/main/java/com/udemy/cursomc/services/PedidoService.java
+++ src/main/java/com/udemy/cursomc/services/PedidoService.java
@@ -13,6 +13,7 @@ import com.udemy.cursomc.domain.enums.EstadoPagamento;
 import com.udemy.cursomc.repositories.ItemPedidoRepository;
 import com.udemy.cursomc.repositories.PagamentoRepository;
 import com.udemy.cursomc.repositories.PedidoRepository;
+import com.udemy.cursomc.repositories.ProdutoRepository;
 import com.udemy.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -33,6 +34,9 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -42,6 +46,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -52,10 +57,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
