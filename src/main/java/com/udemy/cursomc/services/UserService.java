00273f9c57761fdb080e6ad1a4a8c69f6a5062e3
Bruno Zani <ubzani@gmail.com>
2018-06-24 19:10:12 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/udemy/cursomc/services/UserService.java
+++ src/main/java/com/udemy/cursomc/services/UserService.java
@@ -6,7 +6,7 @@ import com.udemy.cursomc.security.UserSS;
 
 public class UserService {
 
-	public static UserSS authenticate() {
+	public static UserSS authenticated() {
 		try {
 			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		}
