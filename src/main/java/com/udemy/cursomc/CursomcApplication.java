9376765296addfb711cf0aec12d01230b99cd2d8
Bruno Zani <ubzani@gmail.com>
2018-06-24 21:20:42 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/udemy/cursomc/CursomcApplication.java
+++ src/main/java/com/udemy/cursomc/CursomcApplication.java
@@ -1,13 +1,19 @@
 package com.udemy.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.udemy.cursomc.services.S3Service;
 
 @SpringBootApplication  // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class CursomcApplication implements CommandLineRunner {  
 		// this interface provides access to application arguments as string array
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -15,5 +21,7 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// (String ...) is an array of parameters of type String, where as String[] is a single parameter
+		
+		s3Service.uploadFile("C:\\tmp\\imgs\\bear.jpg");
 	}
 }
